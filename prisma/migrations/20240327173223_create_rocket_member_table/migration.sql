4fb3bb11e673f2ebd8d8c6c46ffeffa22221349d
Yuri Sousa Oliveira <yuri.sousa42@hotmail.com>
2024-03-29 17:19:53 -0300

    (ADD) MasterClass RocketSeat Project

---
+++ prisma/migrations/20240327173223_create_rocket_member_table/migration.sql
@@ -0,0 +1,6 @@
+-- CreateTable
+CREATE TABLE "RockSeatMember" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "function" TEXT NOT NULL
+);
---
+++ src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { PrismaService } from './database/prisma.service';
+import { RocketMembersRepository } from './repositories/rocket-members-repository';
+import { PrismaRocketMemberRepository } from './repositories/prisma/prisma-rocket-members-repositoriy';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [
+    PrismaService,
+    {
+      provide: RocketMembersRepository,
+      useClass: PrismaRocketMemberRepository,
+    },
+  ],
+})
+export class AppModule {}
